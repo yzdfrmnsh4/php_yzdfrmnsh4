1a27086abdfd206fc636561fcce86dd2198cd77e
Yazid Dev <yaziddev04@gmail.com>
2025-09-05 20:32:15 +0700

    tambah border

--- soal1a.php
+++ soal1a.php
@@ -3,24 +3,28 @@
 // STEP 1: Form awal (input baris & kolom)
 if (!isset($_POST['step'])) {
 ?>
+<div style="border: 1px solid black; padding: 8px; width: 30%;">
+
     <form method="post">
         <div>
-            <label>Inputkan Jumlah Baris:</label>
-            <input type="number" name="baris" required>
+            <label>Inputkan Jumlah Baris :</label>
+            <input type="number" name="baris" required style="margin-bottom: 5px;">
         </div>
         <div>
-            <label>Inputkan Jumlah Kolom:</label>
+            <label>Inputkan Jumlah Kolom : </label>
             <input type="number" name="kolom" required>
         </div>
         <input type="hidden" name="step" value="2">
-        <button type="submit">SUBMIT</button>   
+        <button type="submit" style="margin-top: 5px;">SUBMIT</button>   
     </form>
+</div>
 
 <?php
 // STEP 2: Form input data sesuai baris & kolom
 } elseif ($_POST['step'] == 2) {
     $baris = $_POST['baris'];
     $kolom = $_POST['kolom'];
+    echo "<div style='border: 1px solid black; padding: 8px; '>";
     echo "<form method='post'>";
     for ($i = 1; $i <= $baris; $i++) {
         for ($j = 1; $j <= $kolom; $j++) {
@@ -31,8 +35,9 @@ if (!isset($_POST['step'])) {
     echo "<input type='hidden' name='baris' value='$baris'>";
     echo "<input type='hidden' name='kolom' value='$kolom'>";
     echo "<input type='hidden' name='step' value='3'>";
-    echo "<button type='submit'>SUBMIT</button>";
+    echo "<button type='submit' style='margin-top:5px;'>SUBMIT</button>";
     echo "</form>";
+    echo "</div>";
 
 // STEP 3: Menampilkan hasil input
 } elseif ($_POST['step'] == 3) {
